d1784bef937af0c8c6b35463753cc0ed9b608163
andiblas <andy_blas@hotmail.com>
2018-01-07 17:54:44 -0300

    cambios en la funcionalidad de MathFunctions.sum(). Tests deberian pasar

--- src/main/java/com/ungs/pptwo/MathFunctions.java
+++ src/main/java/com/ungs/pptwo/MathFunctions.java
@@ -3,10 +3,8 @@ package com.ungs.pptwo;
 public class MathFunctions {
 
 	public Integer Sum(Integer a, Integer b) {
-		if (a == null)
-			a = 0;
-		if (b == null)
-			b = 0;
+		if (a == null || b == null)
+			throw new IllegalArgumentException();
 		
 		return a + b;
 	}
